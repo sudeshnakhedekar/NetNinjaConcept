de8e81d77d3f09f2711192465017a0e01b9ea628
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 13:55:15 +0530

    boolean datatype

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,25 @@
-let age;
-console.log(age, age+3, `the age is${age}`);//undefined
+//Booleans & Comparsions
+console.log(true,false, "true","false");
 
-let ages=null;
-console.log(ages, ages+3 ,`the age is ${ages}`);// null
+//methods can return booleans
+let email = 'sudukhedekar@gmail.com'
+let result=email.includes('@')
+console.log(result);
+
+let name = ['meg','leg','peg'];
+let result1= name.includes('meg')
+console.log(result1);
+
+//comparision operators
+let age = 25;
+ console.log(age == 30);
+ console.log(age == 25);
+ console.log(age > 20);
+ console.log(age < 20);
 
 
+ let name1 = 'shuru'
+ console.log( name1 == 'shuru');
+ console.log(name1 == 'bobby' );
+ console.log(name1 > 'Shruu');
+ console.log(name1 > 'Bobby');//lowercase is greater the uppercase
