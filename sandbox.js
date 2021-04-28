824bd49be2df8846596d1268bdc039d89dbac978
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 12:06:49 +0530

    number datatype

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,36 @@
-// common String methods
+let radius =10;
+const pi = 3.14;
 
-let email = 'sudukhedekar@gmail.com';
+//console.log(radius, pi);
 
-//let result = email.lastIndexOf('u');
-//let result = email.slice(0,5);
-// let result = email.substr(4,5);
-let result = email.replace('u','o');//replace only the first 'u'
+//Math Operator +, -, *, /, %, **.
+
+let result = 5^3;
 console.log(result);
 
+let result1 = radius % 3;
+console.log(result1);
+
+// order of operation - B I D M A S
+
+let result2 = 2*(5-3)**2;
+console.log(result2);
+
+//increment and decrement in math
+let likes = 10;
+//likes++;
+//likes--;
+//likes +=10;
+//likes -=5;
+//likes *=2;
+console.log(likes);
+
+
+//NAN
+
+ //concatinate
+ let result3 = 'the blog has ' + likes + ' likes'
+ console.log(result3);
 
 
 
