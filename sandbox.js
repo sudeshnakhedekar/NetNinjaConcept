5a1e2e4259fe295c0c6c45c622ea9c614def7bc1
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 14:19:40 +0530

    type conversion

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,19 @@
-let age = 25;
-
-//loose comaparsion (different types can still be equal)
- console.log(age == 25);
- console.log(age =='25');
-
- console.log(age != 25);
- console.log(age !='25');
+//type conversion
+let score ='100';
+console.log(  score +1);
+score = Number(score)
+console.log(  score +1 );
 
 
- //strict comparsion (different types cannot be equal)
- 
- console.log(age === 25);
- console.log(age ==='25');
-
- console.log(age !== 25);
- console.log(age !=='25');
+let result= Number('hello')
+console.log( typeof result);
+console.log(result);
+let result1 = String(50);
+console.log( typeof result1);
+console.log(result1);
+let result2= Boolean(100);//if value is 0 print false value.
+console.log( typeof result2);
+console.log(result2);
+let result3 = Boolean('0');
+console.log( typeof result3);//' ' will print false as value.
+console.log(result3);
