8765be6b848befdb2bb840b00d18728b26e0c35e
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 14:03:36 +0530

    loose and scrit comparsion

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,17 @@
-//Booleans & Comparsions
-console.log(true,false, "true","false");
-
-//methods can return booleans
-let email = 'sudukhedekar@gmail.com'
-let result=email.includes('@')
-console.log(result);
-
-let name = ['meg','leg','peg'];
-let result1= name.includes('meg')
-console.log(result1);
-
-//comparision operators
 let age = 25;
- console.log(age == 30);
+
+//loose comaparsion (different types can still be equal)
  console.log(age == 25);
- console.log(age > 20);
- console.log(age < 20);
+ console.log(age =='25');
+
+ console.log(age != 25);
+ console.log(age !='25');
 
 
- let name1 = 'shuru'
- console.log( name1 == 'shuru');
- console.log(name1 == 'bobby' );
- console.log(name1 > 'Shruu');
- console.log(name1 > 'Bobby');//lowercase is greater the uppercase
+ //strict comparsion (different types cannot be equal)
+ 
+ console.log(age === 25);
+ console.log(age ==='25');
+
+ console.log(age !== 25);
+ console.log(age !=='25');
