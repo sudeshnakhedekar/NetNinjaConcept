88503037b9ec1ff93cf85fc022701d493639df71
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 13:27:29 +0530

    array methods

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,41 @@
-const title = 'Romeo and Juliet '
-const author = 'Sheikhspeare'
-const follower = 10000000000;
+let ninjas =['shaun ' , 'ryu' , 'chun-li'];
 
-//concatenation string way
-let result = ' The Story named ' + title + ' by ' +author+ ' has ' +follower+ ' follower.';
+// ninjas[1]='joh'
+// console.log(ninjas[1]); //----1
+
+let ages= [20,30,40,50];
+// console.log(ages); //----2
+let resAge = ages.splice(1,2)
+console.log(resAge);//removes specific values
+console.log(ages);
+
+// let random = ['shaun', 'crystal' , 20 , 30];
+// console.log(random); //----3
+
+// console.log(ninjas.length); //----4
+
+//ARRAY METHODS!!!
+
+let result = ninjas.join(',');
 console.log(result);
 
-//template string way
-let result1 = `The Story named ${title} by ${author} has ${follower} follower.`;
+let result1 = ninjas.indexOf('ryu');
 console.log(result1);
 
-//creating HTML templates
-let HTML=`
-<h2>${title}</h2>
-<p> ${author}</p>
-<span>The story has ${follower} follower</span>
-`;
+let result2 =ninjas.concat(['ken', 'crystal']);
+console.log(result2);
 
-console.log(HTML);
+let result3 =ninjas.push('ken');
+console.log(result3);
+console.log(ninjas);//alter the values in ninjas variable.
+
+let result4 =ninjas.pop();
+console.log(result4);
+console.log(ninjas);//alter the values in ninjas variable.
+
+let result5 =ninjas.splice(1,1);
+console.log(result5);//remove specific elment from the list
+console.log(ninjas);
+
+
 
