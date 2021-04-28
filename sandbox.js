55ce0a7b18d688d39abe5ff51c079ad45e6b1c97
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-28 10:48:24 +0530

    initial commit

---
+++ sandbox.js
@@ -0,0 +1,16 @@
+alert('hello, world');
+
+let age = 25;
+let year= 2000;
+console.log(age, year);
+
+age= 30;
+console.log(age);
+
+const point = 100;
+console.log(point);
+
+var score = 50;
+console.log(score);
+score=100;
+console.log(score);
