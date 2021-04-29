b8f4ca16e3b695bb2f99e211ef399f581b158d0d
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-29 13:04:00 +0530

    Variable and block scope

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,29 @@
-//parameters  & arguments
- const speak = function(name= 'billo',time='night'){
-     console.log(`good ${time} ${name}`);
- }
- speak();
-  speak('sudu','morning');
+//returning values
+
+//  const calArea = function(radius){
+//      let area = 3.14*radius**2;
+//     console.log(area);
+//     };
+//     calArea(5);
+
+//instead of above we have do bekow to return the value.
+const calArea = function(radius){
+    let area = 3.14*radius**2;
+  return area;
+   };
+  const a= calArea(5);
+  console.log(a);
+
+  //or another simplify way is:
+  const calArea1 = function(radius){
+  return 3.14*radius**2;
+  
+   };
+   const a1= calArea1(5);
+  console.log(a1);
+
+ 
+
+  
+    
+ 
