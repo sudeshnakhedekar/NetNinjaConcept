2af7032387181a44de925afeaffc6449b29db4e7
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-29 10:30:29 +0530

    Variable and block scope

--- sandbox.js
+++ sandbox.js
@@ -1,45 +1,22 @@
-//switch staements
-const grade ='A';
+// variables & block scope
+let age = 30;
+//const is used when you want to keep the value fix
+if(true){
+    let age = 40;//local variable scope
+    let name = 'shaun';
+    console.log('inside 1st code block : ' , age ,name);
 
-switch(grade){
-    case 'A':
-        console.log("you got an A! grade");
-        break;
-    case 'B':
-        console.log("you got an B! grade");
-        break;
-    case 'C':
-        console.log("you got an C! grade");
-        break;
-    case 'D':
-        console.log("you got an D! grade");
-        break; 
-    case 'E':
-        console.log("you got an E! grade");
-        break; 
-    default:
-        console.log('not a valid grade!');      
+    if(true){
+        let age = 50;
+        console.log('inside 2st code block : ' , age );
+        var test = 'hello'// var is global variable scope
+    }
+
 }
+console.log('outside code block : ' , age ,name, test);
 
 
 
-
-
-//using if statements
-//NOTE: JUST TOL UNDERSTAND HERE
-// if(grade === 'A'){
-
-// }else if (grade === 'B'){
-
-// }else if (grade === 'C'){
-
-// }else if (grade === 'D'){
-
-// }else if (grade === 'E'){
-
-// }else{
-    
-// }
 
 
 
