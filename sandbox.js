e42135dbcecf22c2aa4656121bdf7f3bb200c05d
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-29 12:05:14 +0530

    Function declaration vs function expresssion

--- sandbox.js
+++ sandbox.js
@@ -1,19 +1,23 @@
-// variables & block scope
-let age = 30;
-//const is used when you want to keep the value fix
-if(true){
-    let age = 40;//local variable scope
-    let name = 'shaun';
-    console.log('inside 1st code block : ' , age ,name);
+//function declaration ----> hosting is possible here
 
-    if(true){
-        let age = 50;
-        console.log('inside 2st code block : ' , age );
-        var test = 'hello'// var is global variable scope
-    }
+greet();
+greet();
+greet();
+function greet(){
 
+    console.log('hello there');
 }
-console.log('outside code block : ' , age ,name, test);
+
+
+//function expression ------>  hosting is not possible here
+
+const speak = function(){
+    console.log('good day!');
+}
+
+speak();
+speak();
+speak();
 
 
 
