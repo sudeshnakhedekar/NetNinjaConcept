1c0feda8bd250c1b8d253d7f939b1559918aad85
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-30 18:52:05 +0530

    getAttribute & setAttribute

--- sandbox.js
+++ sandbox.js
@@ -1,45 +1,11 @@
-//####1####
-// const para = document.querySelector('p');
-// console.log(para);
-// console.log(para.innerText);
+const link=document.querySelector('a')
+console.log(link.getAttribute('href'));
+link.setAttribute('href','https://github.com/sudeshnakhedekar');
+link.innerText ='Github website';
+link.setAttribute('style','color: red');
 
-
-//update the text:
-
-//1---append the text ie. add new data with the old data
-// para.innerText +=' ninjas are awsome!';
-
-//2----overwrite the first value.
-// para.innerText =' ninjas are awsome!';
-
-
-//####2####
-//forEach to update the value:
-
-// const paras = document.querySelectorAll('p')
-
-// paras.forEach(para =>{
-//     console.log(para.innerText);
-//     para.innerText += ' new text';
-// })
-
-
-//###3####
-//To change HTML
-
-const content = document.querySelector('.content');
-// console.log(content.innerHTML);
-
-//1----for append.
-// content.innerHTML +='<h2>This is a new h2 </h2>';
-
-//2---- To overwrite.
-// content.innerHTML ='<h2>This is a new h2 </h2>';
-
-
-//list of array.
-const people =['shaun','luigi','yoshi'];
-
-people.forEach(person =>{
-    content.innerHTML += `<p>${person}</p>`
-})
+const mssg = document.querySelector('p')
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class','success');
+mssg.setAttribute('style','color: green');
+mssg.innerText='heeloooo ji'; 
