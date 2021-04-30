b83283cea34cd77951dbd8b00333ee122981058d
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-30 14:41:28 +0530

    innerText and innerHTML

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,45 @@
- //get and element by ID
- const title = document.getElementById('page-title')
- console.log(title);
-
-//get elements by their class name
-const errors = document.getElementsByClassName('error')
-console.log(errors);
- 
-//get elements by their  tag name
-
-const paras = document.getElementsByTagName('p')
-console.log(paras);
+//####1####
+// const para = document.querySelector('p');
+// console.log(para);
+// console.log(para.innerText);
 
 
+//update the text:
+
+//1---append the text ie. add new data with the old data
+// para.innerText +=' ninjas are awsome!';
+
+//2----overwrite the first value.
+// para.innerText =' ninjas are awsome!';
 
 
+//####2####
+//forEach to update the value:
+
+// const paras = document.querySelectorAll('p')
+
+// paras.forEach(para =>{
+//     console.log(para.innerText);
+//     para.innerText += ' new text';
+// })
 
 
+//###3####
+//To change HTML
+
+const content = document.querySelector('.content');
+// console.log(content.innerHTML);
+
+//1----for append.
+// content.innerHTML +='<h2>This is a new h2 </h2>';
+
+//2---- To overwrite.
+// content.innerHTML ='<h2>This is a new h2 </h2>';
+
+
+//list of array.
+const people =['shaun','luigi','yoshi'];
+
+people.forEach(person =>{
+    content.innerHTML += `<p>${person}</p>`
+})
