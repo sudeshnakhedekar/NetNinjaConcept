a7f869c6ccb259363c2a35c413c35f639c73b4ba
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-04-30 10:00:57 +0530

    primitive and reference values

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,22 @@
-//Math object
 
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+//primitive values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+console.log(`scoreOne: ${scoreOne}`,`scoreTwo: ${scoreTwo}`);
+
+scoreOne=100;
+console.log(`scoreOne: ${scoreOne}`,`scoreTwo: ${scoreTwo}`);
+
+//Reference values
+
+const userOne ={name:'ryu', age:20};
+const userTwo =userOne;
+console.log(userOne, userTwo);
+userOne.age =40;
+console.log(userOne, userTwo);
 
 
-const area =7.7;
 
-console.log(Math.round(area));// round up to and integer 
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
-
-//random numbers
-
-const random = Math.random();
-console.log(random);
-console.log(Math.round(random));//0 0r 1 is round up
-console.log(Math.round(random *100));//1 to 100 can be print randomly
 
 
 
