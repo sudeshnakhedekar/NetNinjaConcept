593e680f53248fda8bb629cbeaa1e33a55e557c1
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-02 12:12:36 +0530

    callback hell

--- sandbox1.js
+++ sandbox1.js
@@ -11,7 +11,8 @@ const request = new XMLHttpRequest();
 request.addEventListener('readystatechange',() =>{
     //console.log(request, request.readyState);
     if(request.readyState === 4 && request.status === 200){
-      callback(undefined,request.responseText);
+        const data =JSON.parse(request.responseText)
+      callback(undefined,data);
         //  console.log(request, request.responseText);
     }else if(request.readyState ===4){
       callback('could not fetch the data' ,undefined);  
