61bc25451efc9b4ccd2d7b48e3e75f774ec35ed4
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-02 21:14:27 +0530

    fetch api

--- sandbox1.js
+++ sandbox1.js
@@ -1,73 +1,11 @@
+//fetch api
 
-const getTodos = (resource,callback) =>{
-
-return new Promise((resolve , reject) =>{
-
-    //http request --> to get some external data via kind of api endpoint
-
-// XMLHttpRequest -->this create us a request object .
-const request = new XMLHttpRequest();
-
-//track progress of request using 'event listener & specifiv event caalled ready state change.
-request.addEventListener('readystatechange',() =>{
+fetch('todos/luigi.json').then((response) =>{
+console.log('resolved:' ,response);
+return response.json();
+}).then((data) =>{
+  console.log(data);
+}).catch((error) =>{
+console.log('recjected:',error);
+});
  
-    if(request.readyState === 4 && request.status === 200){
-        const data =JSON.parse(request.responseText)
-      resolve(data);
-      
-    }else if(request.readyState ===4){
-      reject('error getting resource');
-        
-    }
-});
-
-request.open('GET' , resource);
-request.send();
-
-
-});
-
-};
-
-getTodos('todos/luigi.json').then((data) =>{
-         console.log('Promised 1 resolved:',data);
-         return getTodos('todos/mario.json');
-     }).then(data =>{
-console.log('Promised 2 resolved',data);
-return getTodos('todos/shaun.json');
-   }).then((data) =>{
-console.log('Promised 3 resolved' , data);
-   }).catch((error) =>{
-    console.log('Promised rejected:',error);
-    
-     });
-    
-
-
-
- // example :Promises Concept
-
-//  const getsomethig = ()=>{
-
-//     return new Promise((resovle, reject) =>{
-//         //fetech something
-//      resovle('some data');
-//     //reject('some error');
-//     });
-
-//  }
-
-  
-// //  getsomethig().then((data) =>{
-// //      console.log(data);
-// //  }, (error) =>{
-// // console.log(error);
-
-// //  });
-
-
-// getsomethig().then(data =>{
-// console.log(data);
-// }).catch(error =>{
-//     console.log(error);
-// });
