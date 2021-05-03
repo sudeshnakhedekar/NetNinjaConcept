d23412b0340759f7d0e54854aea517b4ff36b23b
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-03 10:52:44 +0530

    async and await

--- sandbox1.js
+++ sandbox1.js
@@ -1,11 +1,32 @@
-//fetch api
 
-fetch('todos/luigi.json').then((response) =>{
-console.log('resolved:' ,response);
-return response.json();
-}).then((data) =>{
-  console.log(data);
-}).catch((error) =>{
-console.log('recjected:',error);
-});
+
+//async & await
+
+const getTodos = async () =>{
+ const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    // console.log(data);
+    
+    return data;
+};
+
+console.log(1);
+console.log(2);
+
+getTodos()
+.then(data => console.log('resolved:',data));
+
+console.log(3);
+console.log(4);
+
+// fetch('todos/luigi.json').then((response) =>{
+// console.log('resolved:' ,response);
+// return response.json();
+// }).then((data) =>{
+//   console.log(data);
+// }).catch((error) =>{
+// console.log('recjected:',error);
+// });
  
+
+
