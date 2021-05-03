20e87cc452fafe3d0249a6d96f7e02bbc51be2d3
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2021-05-03 10:53:24 +0530

    error created manually

--- sandbox1.js
+++ sandbox1.js
@@ -1,32 +1,38 @@
 
 
+
+///*2
+
+
+
 //async & await
 
 const getTodos = async () =>{
- const response = await fetch('todos/luigi.json');
-    const data = await response.json();
-    // console.log(data);
-    
-    return data;
-};
+  const response = await fetch('todos/luigis.json');
+ if(response.status !==200){
+   throw new Error('cannot fetched the dara');
+ }
 
-console.log(1);
-console.log(2);
-
-getTodos()
-.then(data => console.log('resolved:',data));
-
-console.log(3);
-console.log(4);
-
-// fetch('todos/luigi.json').then((response) =>{
-// console.log('resolved:' ,response);
-// return response.json();
-// }).then((data) =>{
-//   console.log(data);
-// }).catch((error) =>{
-// console.log('recjected:',error);
-// });
+     const data = await response.json();
+     // console.log(data);
+     
+     return data;
+ };
  
+ 
+ getTodos()
+ .then(data => console.log('resolved:',data))
+ .catch(error => console.log('rejected',error.message));
+
+
+
+
+
+
+
+
+
+
+//*/
 
 
